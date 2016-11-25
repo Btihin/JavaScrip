872782954010b545eeafa3fbd1308f56283121e7
Student <trnkto@gmail.com>
2016-11-25 15:30:41 +0100

    posledni den

---
+++ Udalosti.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    var message = "Zdarec";
+    document.getElementById("chk1").onclick = function () {
+        document.getElementById("bt1").onclick = (!this.checked) ? null : 
+            function () {
+                console.log(message);
+            }
+    };
+})();
